2d16fde2bb9c3f7d65b0aab258d18e1f4b991de6
kstewart <kGs030106>
2015-05-28 08:47:39 -0500

    add server

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('client'));
+app.use(express.static('../../'));
 
 var server = app.listen(3000, function() {
     console.log('Express server listening on port ' + server.address().port);
